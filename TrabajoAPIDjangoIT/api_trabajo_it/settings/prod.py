0dadb0f061987381f48a1f07a4cdc34fe2b5d072
samuel <samuel.lopez.perez2004@gmail.com>
2026-05-30 21:32:00 +0200

    docker-compose.yml

--- TrabajoAPIDjangoIT/api_trabajo_it/settings/prod.py
+++ TrabajoAPIDjangoIT/api_trabajo_it/settings/prod.py
@@ -7,6 +7,12 @@ DEBUG = False # Seguridad obligatoria en producción
 ALLOWED_HOSTS = ['*'] # En un entorno real, aquí iría el dominio (ej. 'midominio.com')
 
 # Configuración de PostgreSQL leyendo del SO (Inyectado por Docker)
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+]
 
 DATABASES = {
 
@@ -14,15 +20,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': os.getenv('POSTGRES_DB_BLOG'),
+        'NAME': os.getenv('POSTGRES_DB_RESTEMPLATE'),
 
-        'USER': os.getenv('POSTGRES_USER_BLOG'),
+        'USER': os.getenv('POSTGRES_USER_RESTEMPLATE'),
 
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD_BLOG'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD_RESTEMPLATE'),
 
-        'HOST': os.getenv('POSTGRES_HOST_BLOG'),
+        'HOST': os.getenv('POSTGRES_HOST_RESTEMPLATE'),
 
-        'PORT': os.getenv('POSTGRES_PORT_BLOG'),
+        'PORT': os.getenv('POSTGRES_PORT_RESTEMPLATE'),
 
     }
 
--- TrabajoDjangoIT/trabajo_it/settings/prod.py
+++ TrabajoDjangoIT/trabajo_it/settings/prod.py
@@ -2,11 +2,17 @@ import os
 
 from .base import *
 
-DEBUG = False # Seguridad obligatoria en producción
+DEBUG = True # Seguridad obligatoria en producción
 
 ALLOWED_HOSTS = ['*'] # En un entorno real, aquí iría el dominio (ej. 'midominio.com')
 
 # Configuración de PostgreSQL leyendo del SO (Inyectado por Docker)
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+]
 
 DATABASES = {
 
@@ -14,15 +20,15 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': os.getenv('POSTGRES_DB_BLOG'),
+        'NAME': os.getenv('POSTGRES_DB_BEATEMPLATE'),
 
-        'USER': os.getenv('POSTGRES_USER_BLOG'),
+        'USER': os.getenv('POSTGRES_USER_BEATEMPLATE'),
 
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD_BLOG'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD_BEATEMPLATE'),
 
-        'HOST': os.getenv('POSTGRES_HOST_BLOG'),
+        'HOST': os.getenv('POSTGRES_HOST_BEATEMPLATE'),
 
-        'PORT': os.getenv('POSTGRES_PORT_BLOG'),
+        'PORT': os.getenv('POSTGRES_PORT_BEATEMPLATE'),
 
     }
 
